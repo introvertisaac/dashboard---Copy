131128327aa680d236a5fc338e3dcce4efac0c57
Godfrey Makori <godfreykori@gmail.com>
2024-09-22 05:24:08 +0300

    double alert bug fix

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -2,10 +2,12 @@
     <div class="block-warning type-custom w-full fs-4 my-3">
         {!! session('message')  !!}
     </div>
+        <?php session()->forget('message'); ?>
 @endif
 
 @if (session()->has('error'))
     <div class="block-warning type-danger w-full fs-4 my-3">
         {!!  session('error') !!}
     </div>
+        <?php session()->forget('error'); ?>
 @endif
